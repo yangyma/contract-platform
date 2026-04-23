2f98285acf4cbea9c191ff7430a66651a1beb6d8
yangyma <myyjason@gmail.com>
2026-04-23 11:01:24 +0800

    修复数据库鉴权秘钥

--- src/supabaseClient.js
+++ src/supabaseClient.js
@@ -1,6 +1,6 @@
 import { createClient } from '@supabase/supabase-js';
 
 const supabaseUrl = 'https://mgimdwqobjdfltwyjuba.supabase.co';
-const supabaseKey = 'sb_publishable_iZ6eUxi83LV2Uv-eLGdm3A_EyjuInA2';
+const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im1naW1kd3FvYmpkZmx0d3lqdWJhIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzY4OTk4NTQsImV4cCI6MjA5MjQ3NTg1NH0.NrWutPhUqlKDTYW4RwkpxD7xCz9CYWGwUEiqy6iAd84';
 
 export const supabase = createClient(supabaseUrl, supabaseKey);
